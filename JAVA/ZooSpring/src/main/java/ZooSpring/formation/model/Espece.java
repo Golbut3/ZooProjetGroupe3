7f86fe955384ab27813ed9958544a61f8cac830f
Dagmar <cynthia.ehrhard@gmail.com>
2023-09-11 15:45:52 +0200

    commit Cynthia

--- JAVA/ZooSpring/src/main/java/ZooSpring/formation/model/Espece.java
+++ JAVA/ZooSpring/src/main/java/ZooSpring/formation/model/Espece.java
@@ -2,6 +2,7 @@ package ZooSpring.formation.model;
 
 import java.util.List;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -16,7 +17,7 @@ public class Espece {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
-	
+	@Column(length=80)
 	private String nom;
 	
 	@Version
