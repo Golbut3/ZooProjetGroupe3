bc8063494f3ae0b9f6f43bd0bb77d8e98bf30b7c
thrmed <tahirmedjadba@gmail.com>
2023-09-25 15:17:09 +0200

    enclos components

--- Angular/angularZoo/src/app/enclos/enclos.component.ts
+++ Angular/angularZoo/src/app/enclos/enclos.component.ts
@@ -5,11 +5,7 @@ import { EnclosHttpService } from './enclos-http.service';
 import { ChaletHttpService } from '../chalet/chalet-http.service';
 import  {AnimalHttpService } from '../animal/animal-http.service';
 import { InteretHttpService } from '../interet/interet-http.service';
-<<<<<<< Updated upstream
 import { FormBuilder, FormGroup } from '@angular/forms';
-=======
-import { FormGroup } from '@angular/forms';
->>>>>>> Stashed changes
 @Component({
   selector: 'app-enclos',
   templateUrl: './enclos.component.html',
@@ -17,10 +13,7 @@ import { FormGroup } from '@angular/forms';
 })
 export class EnclosComponent {
 
-<<<<<<< Updated upstream
 
-=======
->>>>>>> Stashed changes
   enclosForm: FormGroup;
   showForm:boolean = false;
   encloss$: Observable<Enclos[]>;
@@ -34,10 +27,6 @@ export class EnclosComponent {
   chalets : Array<Chalet>
 
 
-<<<<<<< Updated upstream
-=======
-
->>>>>>> Stashed changes
 
   constructor(private enclosHttpService: EnclosHttpService, private chaletHtttpService: ChaletHttpService,private formBuilder: FormBuilder, private animauxHttpService :AnimalHttpService, private interetsHttpService:InteretHttpService){
     // this.encloss$ = this.enclosHttpService.findAll()
@@ -69,7 +58,6 @@ export class EnclosComponent {
    }
   add(){
     this.enclosForm.reset();
-<<<<<<< Updated upstream
     this.showForm=true;
     
   }
@@ -102,29 +90,6 @@ export class EnclosComponent {
     let enclos : any = this.enclosForm.value;
 
     
-=======
-    this.showForm=true
-  }
-
-  edit(id: number) {
-    this.enclosHttpService.findById(id).subscribe(resp => {
-      this.enclosForm.patchValue(resp);
-      this.showForm=true;
-    //   if(!this.enclosForm.chalets) {
-    //     this.enclosForm.chalets = new Array<Chalet>();
-    //   }
-
-    //   if(!this.enclosForm.animaux){
-    //       this.enclosForm.animaux = new Array<Animal>();
-    //   }
-
-    //   if(!this.enclosForm.animaux){
-    //     this.enclosForm.interets = new Array<Interet>();
-    // }
-
-     });
-  }
->>>>>>> Stashed changes
 
     }
 
