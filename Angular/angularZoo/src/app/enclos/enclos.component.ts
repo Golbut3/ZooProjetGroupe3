5ee29ca02af3e177e3bafa0c4d093590e2d7a0cc
thrmed <tahirmedjadba@gmail.com>
2023-09-25 14:53:14 +0200

    Suite enclos

--- Angular/angularZoo/src/app/enclos/enclos.component.ts
+++ Angular/angularZoo/src/app/enclos/enclos.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, filter } from 'rxjs';
 import { Animal, Chalet, Enclos, Interet } from '../model';
 import { EnclosHttpService } from './enclos-http.service';
 import { ChaletHttpService } from '../chalet/chalet-http.service';
 import  {AnimalHttpService } from '../animal/animal-http.service';
 import { InteretHttpService } from '../interet/interet-http.service';
+import { FormBuilder, FormGroup } from '@angular/forms';
 @Component({
   selector: 'app-enclos',
   templateUrl: './enclos.component.html',
@@ -13,65 +14,92 @@ import { InteretHttpService } from '../interet/interet-http.service';
 export class EnclosComponent {
 
 
+  enclosForm: FormGroup;
+  showForm:boolean = false;
   encloss$: Observable<Enclos[]>;
 
   animaux$: Observable<Animal[]>;
 
   chalets$: Observable<Chalet[]>;
 
-  interets$:Observable<Interet[]>
+  interets$:Observable<Interet[]>;
+  animaux : Array<Animal>;
+  chalets : Array<Chalet>
 
 
-  enclosForm: Enclos = new Enclos();
 
-  constructor(private enclosHttpService: EnclosHttpService, private chaletHtttpService: ChaletHttpService, private animauxHttpService :AnimalHttpService, private interetsHttpService:InteretHttpService){
-     
+  constructor(private enclosHttpService: EnclosHttpService, private chaletHtttpService: ChaletHttpService,private formBuilder: FormBuilder, private animauxHttpService :AnimalHttpService, private interetsHttpService:InteretHttpService){
+    // this.encloss$ = this.enclosHttpService.findAll()
+    // this.chalets$ = this.chaletHtttpService.findAllForAsync()
+    //this.animaux$ = this.animauxHttpService.findAllForAsync()
+    // this.interets$= this.interetsHttpService.findAllForAsync()
   }
 
   ngOnInit():void{
-  this.encloss$ = this.enclosHttpService.findAll();
-  this.chalets$ = this.chaletHtttpService.findAllForAsync();
-  this.animaux$ = this.animauxHttpService.findAllForAsync();
-  this
-  }
+
+  this.enclosForm = this.formBuilder.group({
+  id: this.formBuilder.control(''),
+  capacite: this.formBuilder.control(''),
+  chalets: this.formBuilder.control(''),
+  animaux: this.formBuilder.control('')
+  });}
   
+  list(): Array<Enclos> {
+   
+    return this.enclosHttpService.findAll();
+  }
+  list2(enc: Enclos): Array<Animal> {
+   let animaux =this.animauxHttpService.findAll().filter(a => a.enclos.id == enc.id);
+  return  animaux;
+  }
+  list3(): Array<Animal> {
+  
+   return  this.animauxHttpService.findAll();
+   }
   add(){
-    this.enclosForm = new Enclos();
-    this.enclosForm.chalets = new Array <Chalet>();
-    this.enclosForm.animaux = new Array<Animal>();
-    //this.enclosForm.interets = new <Interet>();
+    this.enclosForm.reset();
+    this.showForm=true;
+    
   }
 
   edit(id: number) {
-    this.enclosHttpService.findById(id).subscribe(resp => {
-      this.enclosForm = resp;
+    this.showForm=true;
+    this.enclosHttpService.findById(id).subscribe(response => {
+      this.enclosForm.patchValue(response);
+      
 
-      if(!this.enclosForm.chalets) {
-        this.enclosForm.chalets = new Array<Chalet>();
-      }
 
-      if(!this.enclosForm.animaux){
-          this.enclosForm.animaux = new Array<Animal>();
-      }
+      const selectedChaletId = response.chalets ? response.chalets: null;
+      this.enclosForm.get('chalets').setValue(selectedChaletId);
+      // const selectedClientId = response.client ? response.client.id : null;
+      // this.reservationForm.get('client').setValue(selectedClientId);
+      // if(!this.enclosForm.value.chalets) {
+      //   this.enclosForm.value.chalets = new Array<Chalet>()};
+  
 
-      if(!this.enclosForm.animaux){
-        this.enclosForm.interets = new Array<Interet>();
+      // if(!this.enclosForm.value.animaux){
+      //     this.enclosForm.value.animaux = new Array<Animal>()};
+      
+
+      // if(!this.enclosForm.value.interets){
+      //   this.enclosForm.value.interets = new Array<Interet>()};
+    })
+  };
+
+  save(){
+    let enclos : any = this.enclosForm.value;
+
+    
+
     }
 
-    });
-  }
-
-  save(){this.enclosHttpService.save(this.enclosForm).subscribe(resp => {
-    this.encloss$ = this.enclosHttpService.findAll();
-  });}
-
-  cancel(){this.enclosForm = new Enclos(0);
+  cancel(){
+    this.showForm=false;
+    this.enclosForm.reset();
   }
 
   remove(id:number){
-    this.enclosHttpService.deleteById(id).subscribe(resp => {
-      this.encloss$ = this.enclosHttpService.findAll();
-    });
+    this.enclosHttpService.deleteById(id);
 
   }
 
